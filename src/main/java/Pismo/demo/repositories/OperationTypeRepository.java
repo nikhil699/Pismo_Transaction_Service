ee74d3233275b731b8fa135c626103ac7ba68e0a
nikhilchaurasiya <chaurasiyanikhil744@gmail.com>
2025-08-29 13:23:06 +0530

    release: changes done in the  format in the  document to understand flow correctly..

--- src/main/java/Pismo/demo/repositories/OperationTypeRepository.java
+++ src/main/java/Pismo/demo/repositories/OperationTypeRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 
 public interface OperationTypeRepository extends JpaRepository<OperationType, Short> {
     Optional<Account> findByDocumentNumber(String documentNumber);
+
 }
